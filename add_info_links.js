7722c211785db05101cb01472759f8f8224967fb
bubbleupdotnet <admin@bubbleup.net>
2022-02-08 10:58:17 -0600

    Update add_info_links.js
    Changing name

--- add_info_links.js
+++ add_info_links.js
@@ -1,5 +1,5 @@
 // ==UserScript==
-// @name         TeamWork show missing time
+// @name         FreshDesk Add Info Links
 // @namespace    https://www.bubbleup.net/
 // @version      0.1
 // @description  Adds links in FreshDesk tickets to search customer/user/orders in Shopify and Connect based on the email address in the ticket.
