b40df3f98a7e79d71e4bafccb5a42ba47a8f0672
bubbleupdotnet <admin@bubbleup.net>
2022-02-08 10:57:08 -0600

    Create add_info_links.js
    adding new file

---
+++ add_info_links.js
@@ -0,0 +1,13 @@
+// ==UserScript==
+// @name         TeamWork show missing time
+// @namespace    https://www.bubbleup.net/
+// @version      0.1
+// @description  Adds links in FreshDesk tickets to search customer/user/orders in Shopify and Connect based on the email address in the ticket.
+// @author       Joe
+// @match        https://pm.bubbleup.net/*
+// @grant        none
+// ==/UserScript==
+
+(function() {
+
+})();
